675311844bfd335a47587a26c6d61736bfcbc502
JohnNafuh <johnnafuh@gmail.com>
2026-05-17 15:15:56 +0100

    Update header.js

--- header.js
+++ header.js
@@ -1,18 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".site-header").innerHTML = `
-    <header class="top-header">
 
-     <div class="logo" onclick="location.href='index.html'">
-  <img src="9468D5BC-D608-461F-8FD0-D934C8A0A490.png" alt="CrownCapital Logo">
-</div>
+  const header = document.querySelector(".site-header");
+
+  if (!header) return;
+
+  header.innerHTML = `
+    <header class="topbar">
+
+      <div class="logo" onclick="location.href='index.html'">
+        <img src="9468D5BC-D608-461F-8FD0-D934C8A0A490.png" alt="CrownCapital">
+      </div>
 
       <nav class="nav-links">
         <a href="index.html">Home</a>
-        <a href="plans.html">Investment Plans</a>
+        <a href="plans.html">Plans</a>
         <a href="dashboard.html">Dashboard</a>
         <a href="wallet.html">Wallet</a>
       </nav>
 
     </header>
   `;
+
+  // ACTIVE TAB SYSTEM
+  const links = document.querySelectorAll(".nav-links a");
+  let currentPage = window.location.pathname.split("/").pop();
+
+  if (currentPage === "" || currentPage === "/") {
+    currentPage = "index.html";
+  }
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+
 });
