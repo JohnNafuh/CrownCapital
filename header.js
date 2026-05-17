d3e861acca9add364e983db4f1183fa3bbf2f998
JohnNafuh <johnnafuh@gmail.com>
2026-05-17 14:55:41 +0100

    Update header.js

--- header.js
+++ header.js
@@ -2,18 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".site-header").innerHTML = `
     <header class="top-header">
 
-      <div class="logo" onclick="location.href='index.html'">
-
-        <div class="logo-icon">
-          <svg width="26" height="26" viewBox="0 0 24 24" fill="none">
-            <path d="M3 7L7 11L12 5L17 11L21 7V18C21 19.1 20.1 20 19 20H5C3.9 20 3 19.1 3 18V7Z"
-              stroke="#b08d2a" stroke-width="1.8" stroke-linejoin="round"/>
-          </svg>
-        </div>
-
-        <div class="logo-text">CrownCapital</div>
-
-      </div>
+     <div class="logo" onclick="location.href='index.html'">
+  <img src="9468D5BC-D608-461F-8FD0-D934C8A0A490.png" alt="CrownCapital Logo">
+</div>
 
       <nav class="nav-links">
         <a href="index.html">Home</a>
